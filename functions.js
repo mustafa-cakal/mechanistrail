760af2742e6cbc3390590ec4424e368dbceacea1
mustafa-cakal <mstfckl07.mc@gmail.com>
2026-05-07 12:55:58 +0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,10 +1,11 @@
-// Sayfa yüklendiğinde konsola bir selamlama gönderir
-window.onload = function() {
-    console.log("Mechanist Rail web sitesine hoş geldiniz!");
-    
-    // Basit bir etkileşim: Sitedeki logoya tıklandığında bir uyarı verir
-    const logo = document.querySelector('.logo');
-    logo.addEventListener('click', function() {
-        alert("Mechanist Rail: Geleceğin Mekaniğini Tasarlıyoruz!");
-    });
-};
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+// 5 saniyede bir lokomotif bir sonraki istasyona (slayta) geçer
+setInterval(nextSlide, 5000);
